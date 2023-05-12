8aad1a0658fc327023f6ac92d7f53b43b5d1f40c
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2023-05-12 15:08:39 -0300

    Implementação da 'BoardException'

--- src/com/luisgmr/chessgame/boardgame/Board.java
+++ src/com/luisgmr/chessgame/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Erro ao criar um tabuleiro, é necessário que haja pelo menos 1 linha e 1 coluna.");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -16,29 +19,45 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Essa posição (" + row + ", " + column + ") não existe no tabuleiro.");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Essa posição (" + position + ") não existe no tabuleiro.");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (isPiece(position)) {
+            throw new BoardException("Já existe uma peça nessa posição (" + position + ").");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean isPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Essa posição (" + position + ") não existe no tabuleiro.");
+        }
+        return piece(position) != null;
     }
 
 }
