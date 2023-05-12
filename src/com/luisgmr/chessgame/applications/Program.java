83afb5e8ffed91bf0713f93545170900c2b9a298
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2023-05-12 17:43:20 -0300

    Exceções no Program

--- src/com/luisgmr/chessgame/applications/Program.java
+++ src/com/luisgmr/chessgame/applications/Program.java
@@ -1,9 +1,11 @@
 package com.luisgmr.chessgame.applications;
 
+import com.luisgmr.chessgame.chess.ChessException;
 import com.luisgmr.chessgame.chess.ChessMatch;
 import com.luisgmr.chessgame.chess.ChessPiece;
 import com.luisgmr.chessgame.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,18 +15,46 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UserInterface.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Posição de origem: ");
-            ChessPosition source = UserInterface.readChessPosition(sc);
-            System.out.println();
-            System.out.print("Posição de destino: ");
-            ChessPosition target = UserInterface.readChessPosition(sc);
+            try {
+                UserInterface.clearScreen();
+                UserInterface.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Posição de origem: ");
+                ChessPosition source = UserInterface.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Posição de destino: ");
+                ChessPosition target = UserInterface.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println("\n" + e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("\n" + e.getMessage());
+                sc.nextLine();
+            }
         }
+    }
 
+    public final static void clearConsole()
+    {
+        try
+        {
+            final String os = System.getProperty("os.name");
 
+            if (os.contains("Windows"))
+            {
+                Runtime.getRuntime().exec("cls");
+            }
+            else
+            {
+                Runtime.getRuntime().exec("clear");
+            }
+        }
+        catch (final Exception e)
+        {
+            //  Handle any exceptions.
+        }
     }
 
 }
